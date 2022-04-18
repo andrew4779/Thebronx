7b92b7261f33b432a39496238cd174a442e5ee09
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-18 14:31:42 +0300

    created neighbourhood model

--- myneighbour/models.py
+++ myneighbour/models.py
@@ -11,3 +11,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    photo = CloudinaryField('image')
+    description = models.TextField()
+    occupants_count= models.IntegerField( default=0, blank=True)
+    health_toll =  models.IntegerField(null=True, blank=True)
+    police_toll =  models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
