e7c7e21a743a14f4387f4d6744f5055b06013f56
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-18 14:07:10 +0300

    add django registration in installed apps

--- myhood/settings.py
+++ myhood/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myneighbour'
+    'myneighbour',
+    'django_registration',
 ]
 
 MIDDLEWARE = [
