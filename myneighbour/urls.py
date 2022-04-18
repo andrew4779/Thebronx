0e3deadd9037a28f7e8a3884f9d7c037660aaeca
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-18 15:58:48 +0300

    update html files

--- myneighbour/urls.py
+++ myneighbour/urls.py
@@ -8,12 +8,13 @@ urlpatterns=[
     path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
     path('profile/', views.profile, name='profile'),
     path('hood/', views.hoods, name='hood'),
-    path('search/',views.search_results,name='search_results'),
-    path('new-hood/', views.new_hood, name='new-hood'),
-    # path('post/<hood_id>', views.post, name='post'),
     path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
     path('join_hood/<id>', views.join_hood, name='join-hood'),
-
-
-
+    path('new-hood/', views.new_hood, name='new-hood'),
+    path('<hood_id>/news', views.news, name='news'),                    
+    path('<hood_id>/business',views.business,name='business'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('businesses/<hood_id>', views.businesses, name='businesses'),
+    path('post/<hood_id>', views.post, name='post'),
+    path('search/',views.search_results,name='search_results'),
 ]
